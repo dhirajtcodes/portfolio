3f08ff6b82b2c7d9f17e57566a77e0ceb55959ea
dtandulkar <dtandulkar1998@gmail.com>
2022-05-12 15:49:29 +0530

    changes

--- src/components/core/header/CTAButton.js
+++ src/components/core/header/CTAButton.js
@@ -5,7 +5,7 @@ import CV from '../../../assets/cv.pdf'
 const CTAButton = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">Download CV</a> 
+      <a href={CV} download className="btn">Resume</a> 
       <a href="tel:8806122305"className="btn btn-primary" onClick="ga('send', 'event', { eventCategory: 'Contact', eventAction: 'Call', eventLabel: 'Mobile Button'});">Let's Talk</a>
     </div>
   )
